8bd9b22553305f7a256c6e85c7a4b407d1c68347
Ojaswi Wadhwa <ojas.vwadhwa1995@gmail.com>
2018-08-08 13:11:49 +0530

    angular routing debugging

--- app/assets/javascripts/angular/admin/app.js
+++ app/assets/javascripts/angular/admin/app.js
@@ -3,51 +3,102 @@ angular.module('myAdmin.services',[]);
 angular.module('myAdmin.directives',[]);
 var app =  angular.module('myAdmin', ['myAdmin.controllers', 'ngRoute', 'ngFileUpload', 'route-segment', 'view-segment']);
 
-app.config(['$routeSegmentProvider', function($routeSegmentProvider){
+// app.config(['$routeSegmentProvider', '$routeProvider', function($routeSegmentProvider, $routeProvider){
+
+// 	$routeSegmentProvider.
+// 	when('/', 'app.index').
+// 	when('/blogs', 'app.blogs').
+// 	when('/posts', 'app.posts').
+// 	when('/users', 'app.users')
+
+// 	.segment("app", {
+// 		templateUrl: '/templates/app.html',
+// 		controller: 'appController'
+// 	})
+// 	.within()
+
+// 	.segment('index', {
+// 		templateUrl: '/templates/posts/index.html',
+// 		controller: 'postController'
+// 	})
+// 	.segment('blogs', {
+// 		templateUrl: '/templates/blogs/index.html',
+// 		controller : 'blogsController'
+// 	})
+// 	.segment('posts', {
+// 		templateUrl: '/templates/posts/index.html',
+// 		controller: 'postController'
+// 	})
+// 	.segment('users`', {
+// 		templateUrl: '/templates/users/index.html',
+// 		controller: 'usersController'
+// 	})
+
+// 	$routeProvider.otherwise({ redirectTo: '/' })
+// }])
+
+// app.config(['$routeSegmentProvider','$locationProvider', function($routeSegmentProvider, $locationProvider){
+
+//  	$routeSegmentProvider.
+// 	when('/', 'app.index').
+// 	when('/blogs', 'app.blogs').
+// 	when('/posts', 'app.posts').
+// 	when('/users', 'app.users')
+
+//  	.segment("app", {
+// 		templateUrl: '/templates/app.html',
+// 		controller: 'appController'
+// 	})
+// 	.within()
+
+// 	.segment('index', {
+// 		templateUrl: '/templates/posts/index.html',
+// 		controller: 'postController'
+// 	})
+
+// 	.within()
+
+// 	.segment('blogs', {
+// 		templateUrl: '/templates/blogs/index.html',
+// 		controller: 'blogsController'
+// 	})
+
+// 	.within()
+
+// 	.segment('posts', {
+// 		templateUrl: '/templates/posts/index.html',
+// 		controller: 'postController'
+// 	})
+
+// 	.within()
+
+// 	.segment('users', {
+// 		templateUrl: '/templates/users/index.html',
+// 		controller: 'usersController'
+// 	})
+// }])
+
+app.config(function($routeSegmentProvider){
 
 	$routeSegmentProvider.
-	when('/', 'app.index').
-	when('/blogs', 'app.blogs').
-	when('/posts', 'app.posts').
-	when('/users', 'app.users')
+	when('/', 'app').
+	when('/app/posts', 'app.posts').
+	when('/app/blogs', 'app.blogs').
 
-	.segment("app", {
-		templateUrl: '/templates/app.html',
+	segment('app',{
+		templateUrl: 'templates/app/app.html',
 		controller: 'appController'
-	})
-	.within()
-
-	.segment('index', {
-		templateUrl: '/templates/posts/index.html',
-		controller: 'postController'
-	})
-	.segment('blogs', {
-		templateUrl: '/templates/blogs/index.html',
-		controller : 'blogsController'
+	}).
+	within().
+	segment('blogs',{
+		default: true,
+		templateUrl: 'templates/app/blogs.html',
+		controller: 'blogsController'
 	})
 	.segment('posts', {
-		templateUrl: '/templates/posts/index.html',
-		controller: 'postController'
+		templateUrl: 'templates/app/posts.html'
 	})
-	.segment('users', {
-		templateUrl: '/templates/users/index.html',
-		controller: 'usersController'
-	})
-
-	// $routeProvider.when('/', {
-	// 	templateUrl: '/templates/posts/index.html',
-	// 	controller: 'postController'
-	// }).when('/posts/new', {
-	// 	templateUrl: '/templates/posts/new.html', 
-	// 	controller: 'postController'
-	// }).when('/show', {
-	// 	templateUrl: '/templates/posts/show.html',
-	// 	controller: 'postController'
-	// }).when('/edit', {
-	// 	templateUrl: '/templates/posts/edit.html',
-	// 	controller: 'postController'
-	// })
-}])
+})
 
 app.filter('Demofilter',function(){
   return function(input)
